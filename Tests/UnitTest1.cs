a6b5375781adf8cc484cef9ee01cc5cb1c1ae715
Heber <sengerhe@gmail.com>
2024-01-06 04:57:20 -0300

    retrieve method red

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,65 @@
 namespace Tests;
 
-public class UnitTest1
+public class EventStoreShould
 {
-    [Fact]
-    public void Test1()
+    private EventStore CreateDefaultEventStore()
     {
+        return new EventStore();
+    }
 
+    [Fact]
+    public async Task RetrieveAggregateRootFullyRestored()
+    {
+        var eventStore = CreateDefaultEventStore();
+        var person = new Person(new PersonCreatedEvent("John"));
+        person.UpdateName(new PersonNameUpdatedEvent("John Doe"));
+        await eventStore.Save(person);
+
+        var restoredPerson = await eventStore.Find<Person>(person.Id);
+        Assert.Equal("John Doe", restoredPerson!.Name);
+    }
+}
+
+public class EventStore
+{
+    public async Task<TAggregateRoot?> Find<TAggregateRoot>(object id)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
+
+    public async Task Save(Person person)
+    {
+        await Task.CompletedTask;
+        throw new NotImplementedException();
+    }
+}
+
+public class Person
+{
+    public Guid Id { get; }
+    public string Name { get; private set; }
+    private readonly DateTime _createdAt;
+
+    public Person(PersonCreatedEvent evt)
+    {
+        Id = evt.Id;
+        Name = evt.Name;
+        _createdAt = evt.CreatedAt;
+    }
+
+    public void UpdateName(PersonNameUpdatedEvent evt)
+    {
+        Name = evt.Name;
+    }
+}
+
+public record PersonNameUpdatedEvent(string Name);
+
+public record PersonCreatedEvent(Guid Id, string Name, DateTime CreatedAt)
+{
+    public PersonCreatedEvent(string name)
+        : this(Guid.NewGuid(), name, DateTime.UtcNow)
+    {
     }
 }
