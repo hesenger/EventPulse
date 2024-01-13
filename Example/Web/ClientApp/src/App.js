80e0b18759813bc4f417bf0c254ab0f2b650a7f1
Heber <sengerhe@gmail.com>
2024-01-13 08:20:05 -0300

    fix console errors on react

--- Example/Web/ClientApp/src/App.js
+++ Example/Web/ClientApp/src/App.js
@@ -7,23 +7,30 @@ import "./custom.css";
 export default function App() {
   const [selectedDay, setSelectedDay] = useState();
 
-  const footer = selectedDay ? (
-    <p>{format(selectedDay, "PPP")}.</p>
-  ) : (
-    <p>Please pick a day.</p>
-  );
+  const selection = selectedDay
+    ? format(selectedDay, "PPP")
+    : "Please pick a day.";
 
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <DayPicker
-            mode="single"
-            selected={selectedDay}
-            onSelect={setSelectedDay}
-          />
+    <div className="container">
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        >
+          <span className="fs-4">Hotel manager</span>
+        </a>
+      </header>
+      <div className="d-flex flex-row">
+        <DayPicker
+          mode="single"
+          selected={selectedDay}
+          onSelect={setSelectedDay}
+          showOutsideDays
+        />
+        <div className="flex-fill">
+          <h3>{selection}</h3>
         </div>
-        <div class="col">{footer}</div>
       </div>
     </div>
   );
